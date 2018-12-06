410b5803ecac11550f6f8f76c2764b4d45533e0e
Victor <doctorhow23@googlemail.com>
2018-12-06 18:48:24 +0000

    added a cool text-field with css

--- src/components/ParticipantList.js
+++ src/components/ParticipantList.js
@@ -1,17 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import cyan from '@material-ui/core/colors/cyan';
 
-class ParticipantList extends Component {
-    render() {
-        return (
-            <ol>
-              <li>put</li>
-              <li>participant</li>
-              <li>list</li>
-              <li>component</li>
-              <li>here</li>
-            </ol>
-        );
-    }
+const styles = theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing.unit,
+    },
+    cssLabel: {
+      '&$cssFocused': {
+        color: cyan[500],
+      },
+      color: 'white'
+    },
+    cssFocused: {
+        color: cyan[500]
+    },
+    cssUnderline: {
+        '&:before':{
+            borderBottomColor: 'white'
+          },
+        '&:after': {
+            borderBottomColor: cyan[500],
+        },
+        '&&&&:hover:before':{
+            borderBottom: '2px solid rgba(0, 255, 255, 0.42)'
+        },
+    },
+  });
+
+function ParticipantList(props) { 
+    const { classes } = props;
+    
+    return (
+        <div className={classes.container}>
+            <FormControl className={classes.margin}>
+            <InputLabel
+                htmlFor="custom-css-standard-input"
+                classes={{root: classes.cssLabel, focused: classes.cssFocused,}}>
+                Add Participant
+            </InputLabel>
+            <Input id="custom-css-standard-input" classes={{underline: classes.cssUnderline, focused: classes.cssFocused}} />
+            </FormControl>
+        </div>
+    );
 }
 
-export default ParticipantList;
+ParticipantList.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+export default withStyles(styles)(ParticipantList);
