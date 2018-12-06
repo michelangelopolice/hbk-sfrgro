650893726c14df0aa766340bc6faac708d3d5e4e
Victor <doctorhow23@googlemail.com>
2018-12-06 11:58:28 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={require("./oro.gif")} className="Oro" alt="oro" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Suck my nuts, assholes.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
